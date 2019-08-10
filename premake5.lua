7860b84bf6499a6165b3dc7902b42151a9ca8453
shagufta <shagufta.a@smartvizx>
2019-08-10 14:03:18 +0530

    premake system added

---
+++ premake5.lua
@@ -0,0 +1,111 @@
+workspace "Nutcracker"
+
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Nutcracker"
+	location "Nutcracker"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "c++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+		
+			"NC_PLATFORM_WINDOWS",
+			"NC_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+		filter "configurations:Debug"
+			defines "NC_DEBUG"
+			symbols "On"
+
+		filter "configurations:Release"
+			defines "NC_RELEASE"
+			optimize "On"
+
+		filter "configurations:Dist"
+			defines "NC_DIST"
+			optimize "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Nutcracker/vendor/spdlog/include",
+		"Nutcracker/src"
+	}
+
+	links
+	{
+		"Nutcracker"
+	}
+
+	filter "system:windows"
+		cppdialect "c++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+		
+			"NC_PLATFORM_WINDOWS"
+		}
+
+		
+
+		filter "configurations:Debug"
+			defines "NC_DEBUG"
+			symbols "On"
+
+		filter "configurations:Release"
+			defines "NC_RELEASE"
+			optimize "On"
+
+		filter "configurations:Dist"
+			defines "NC_DIST"
+			optimize "On"
